63287ba10d1c643b9ce6dedf6ebcf97101e3f8aa
marcoaversa <marco.aversa@outlook.com>
2023-09-22 16:37:26 +0100

    inpainting omegas carousel works

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,10 +1,21 @@
 function showCarousel(carouselId) {
     // Hide all images
-    var carousels = document.getElementsByClassName('container');
+    var carousels = document.getElementsByClassName('selected-image');
     for(var i = 0; i < carousels.length; i++) {
         carousels[i].style.display = 'none';
     }
-    
-    // Show the selected image
+
     document.getElementById(carouselId).style.display = 'block';
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function(){
+        document.getElementById('Artery').style.display = 'none';
+        document.getElementById('Artifacts').style.display = 'none';
+        document.getElementById('Carcinoma').style.display = 'none';
+        document.getElementById('Cartilage').style.display = 'none';
+        document.getElementById('Connective').style.display = 'none';
+        document.getElementById('Necrosis').style.display = 'none';
+        document.getElementById('Stroma').style.display = 'none';
+    }, 1); // 1000 milliseconds = 1 second
+  });
