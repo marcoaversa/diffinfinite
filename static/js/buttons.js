6dfe8ab39b74e783df060ebba4ff60ad657f0dcb
marcoaversa <marco.aversa@outlook.com>
2023-09-22 10:57:07 +0100

    test carousel

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,6 +1,6 @@
 function showCarousel(carouselId) {
     // Hide all images
-    var carousels = document.getElementsByClassName('grid');
+    var carousels = document.getElementsByClassName('hero-body');
     for(var i = 0; i < carousels.length; i++) {
         carousels[i].style.display = 'none';
     }
