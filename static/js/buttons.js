8dc1171146abd1da6e182a47683f4b19d363f511
marcoaversa <marco.aversa@outlook.com>
2023-09-22 10:34:09 +0100

    test carousel

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,5 +1,5 @@
 function showCarousel(carouselId) {
-    const carousels = document.getElementsByClassName('container');
+    const carousels = document.getElementsByClassName('hero-body');
     for (let carousel of carousels) {
         carousel.style.display = carousel.id === carouselId ? 'block' : 'none';
     }
