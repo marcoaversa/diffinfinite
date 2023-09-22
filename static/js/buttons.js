387ebe15ff78bb73e027c22cf375ccd96b84c976
marcoaversa <marco.aversa@outlook.com>
2023-09-22 10:04:29 +0100

    test carousel

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,25 +1,9 @@
-function showImage(imageId) {
-    // Remove 'active' class from all images
-    document.querySelectorAll('.image').forEach(img => {
-      img.classList.remove('active');
-    });
-    
-    // Add 'active' class to the selected image
-    document.getElementById(imageId).classList.add('active');
-}
-
 function showCarousel(carouselId) {
     // First, hide all carousels
-    document.querySelectorAll('.carousel').forEach(carousel => {
-        carousel.style.display = 'none';
+    document.querySelectorAll('.grid').forEach(grid => {
+        grid.style.display = 'none';
     });
 
     // Now, show the chosen carousel
     document.getElementById(carouselId).style.display = 'block';
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-// Initialize carousels here
-document.getElementById('carousel1').innerHTML = generateHTML(1);
-document.getElementById('carousel2').innerHTML = generateHTML(2);
-});
