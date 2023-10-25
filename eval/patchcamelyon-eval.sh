655578b174415330798fdb8113772ec649473d88
Luis Oala <luis.oala@dotphoton.com>
2023-10-25 11:29:06 +0200

    Update patchcamelyon-eval.sh

--- eval/patchcamelyon-eval.sh
+++ eval/patchcamelyon-eval.sh
@@ -59,73 +59,73 @@ def preprocess(image, label):
     image = image * 255  # Scale back to [0, 255]
     return image, label
 
+for split in ('train', 'test', 'validation'):    
+    # Loading the patchcamelyon dataset
+    patchcamelyon_data, _ = tfds.load('patch_camelyon', split=split, with_info=True, download=False, as_supervised=True)
+    
+    # Apply preprocessing
+    patchcamelyon_data = patchcamelyon_data.map(preprocess)
+    
+    def create_placeholder_model():
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(96, 96, 3)),
+            tf.keras.layers.MaxPooling2D(2, 2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        return model
+    
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        print("gpu(s) available. tensorflow will use gpu:", gpus[0])
+    else:
+        print("no gpu available. tensorflow will use cpu.")
+    
+    all_predictions = []
+    all_labels = []
+    
+    if tf.io.gfile.exists("$MODEL_DIR"):
+        model = tf.saved_model.load("$MODEL_DIR")
+        infer = model.signatures["pred_fn"]
+        for images, labels in patchcamelyon_data.batch($BATCH_SIZE):
+            batch_predictions = infer(tf.constant(images))['output_0'].numpy()
+            #print("images")
+            #print(images)
+            #print("raw batch")
+            #print(batch_predictions)
+            predicted_class = batch_predictions.argmax(axis=1)
+            all_predictions.extend(predicted_class)
+            #print("preds")
+            #print(predicted_class)
+            all_labels.extend(labels.numpy())
+            #print("gt")
+            #print(labels)
+    else:
+        print("using placeholder model as no saved model was provided.")
+        model = create_placeholder_model()
+        for images, labels in patchcamelyon_data.batch($BATCH_SIZE):
+            batch_predictions = model.predict(images)
+            predicted_class = batch_predictions.argmax(axis=1)
+            all_predictions.extend(predicted_class)
+            all_labels.extend(labels.numpy())
+    
+    #print(numpy.sum(all_labels))
+    #print(numpy.sum(all_predictions))
+    
+    #f1 = f1_score(all_labels, all_predictions)
+    #print(f"f1 score: {f1}")
+    
+    #cm = confusion_matrix(all_labels,all_predictions)
+    
+    #print(cm)
 
-
-# Loading the patchcamelyon dataset
-patchcamelyon_data, _ = tfds.load('patch_camelyon', split='train', with_info=True, download=False, as_supervised=True)
-
-# Apply preprocessing
-patchcamelyon_data = patchcamelyon_data.map(preprocess)
-
-def create_placeholder_model():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(96, 96, 3)),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    return model
-
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    print("gpu(s) available. tensorflow will use gpu:", gpus[0])
-else:
-    print("no gpu available. tensorflow will use cpu.")
-
-all_predictions = []
-all_labels = []
-
-if tf.io.gfile.exists("$MODEL_DIR"):
-    model = tf.saved_model.load("$MODEL_DIR")
-    infer = model.signatures["pred_fn"]
-    for images, labels in patchcamelyon_data.batch($BATCH_SIZE):
-        batch_predictions = infer(tf.constant(images))['output_0'].numpy()
-        #print("images")
-        #print(images)
-        #print("raw batch")
-        #print(batch_predictions)
-        predicted_class = batch_predictions.argmax(axis=1)
-        all_predictions.extend(predicted_class)
-        #print("preds")
-        #print(predicted_class)
-        all_labels.extend(labels.numpy())
-        #print("gt")
-        #print(labels)
-else:
-    print("using placeholder model as no saved model was provided.")
-    model = create_placeholder_model()
-    for images, labels in patchcamelyon_data.batch($BATCH_SIZE):
-        batch_predictions = model.predict(images)
-        predicted_class = batch_predictions.argmax(axis=1)
-        all_predictions.extend(predicted_class)
-        all_labels.extend(labels.numpy())
-
-print(numpy.sum(all_labels))
-print(numpy.sum(all_predictions))
-
-#f1 = f1_score(all_labels, all_predictions)
-#print(f"f1 score: {f1}")
-
-cm = confusion_matrix(all_labels,all_predictions)
-
-print(cm)
-
-ba = sklearn.metrics.balanced_accuracy_score(all_labels,all_predictions)
-print("ba: ", ba)
-
-
-report = classification_report(all_labels, all_predictions)
-print(report)
+    print(split)
+    ba = sklearn.metrics.balanced_accuracy_score(all_labels,all_predictions)
+    print("ba: ", ba)
+    
+    
+    #report = classification_report(all_labels, all_predictions)
+    #print(report)
 END
